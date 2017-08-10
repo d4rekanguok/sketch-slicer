17c921fe6de01e5a92e4d8c7e2d709183082c612
Derek <dikanguok@gmail.com>
2017-08-10 15:47:29 +0900

    basic version

--- slice.sketchplugin/Contents/Sketch/slice.js
+++ slice.sketchplugin/Contents/Sketch/slice.js
@@ -77,8 +77,39 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 exports['default'] = function (context) {
-  context.document.showMessage('It\'s alive 🙌');
+  var doc = context.document;
+  var selected = context.selection;
+
+  selected.forEach(function (layer) {
+    slice(layer, 0);
+  });
+  doc.showMessage('sliced ' + selected.length + ' layers.');
 };
+
+function slice(layer, padding) {
+  //create a group from layer
+  // log(layer.frame())
+  var layers = MSLayerArray.arrayWithLayer(layer);
+  var group = MSLayerGroup.groupFromLayers(layers);
+  group.setName(layer.name() + '_export');
+
+  var slice = MSSliceLayer.sliceLayerFromLayer(layer);
+  slice.setName(layer.name());
+  var frame = slice.frame();
+  var layerFrame = layer.frame();
+  // log(frame)
+  frame.setX(layerFrame.x() - padding);
+  frame.setY(layerFrame.y() - padding);
+  frame.setWidth(layer.frame().width() + padding * 2);
+  frame.setHeight(layer.frame().height() + padding * 2);
+  log(frame);
+  //set slice to only do content
+  slice.exportOptions().setLayerOptions(2);
+  context.document.reloadInspector();
+  group.layerDidEndResize();
+
+  return slice;
+}
 
 /***/ })
 /******/ ]);
--- src/slice.js
+++ src/slice.js
@@ -1,3 +1,34 @@
 export default function (context) {
-  context.document.showMessage('It\'s alive 🙌')
+  const doc = context.document;
+  let selected = context.selection
+
+  selected.forEach(layer => {
+    slice(layer, 0)
+  })
+  doc.showMessage('sliced ' + selected.length + ' layers.')
+}
+
+function slice (layer, padding) {
+  //create a group from layer
+  // log(layer.frame())
+  var layers = MSLayerArray.arrayWithLayer(layer)
+  var group = MSLayerGroup.groupFromLayers(layers)
+  group.setName(layer.name() + '_export')
+
+  var slice = MSSliceLayer.sliceLayerFromLayer(layer)
+  slice.setName(layer.name())
+  var frame = slice.frame()
+  var layerFrame = layer.frame()
+  // log(frame)
+  frame.setX(layerFrame.x() - padding)
+  frame.setY(layerFrame.y() - padding)
+  frame.setWidth(layer.frame().width() + padding*2)
+  frame.setHeight(layer.frame().height() + padding*2)
+  log(frame)
+  //set slice to only do content
+  slice.exportOptions().setLayerOptions(2)
+  context.document.reloadInspector()
+  group.layerDidEndResize()
+
+  return slice
 }
